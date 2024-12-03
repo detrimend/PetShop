bc169947986b8cfe3d6d478fef72d9bd25bd8824
victor <victorgrud@gmail.com>
2024-12-04 00:14:07 +0100

    næsten alt med reservation færdig

---
+++ src/DateInterval.java
@@ -0,0 +1,39 @@
+import javafx.scene.control.DatePicker;
+
+import java.time.LocalDate;
+
+public class DateInterval {
+    private int days;
+    private LocalDate startDate;
+
+    public DateInterval(LocalDate startDate, int days){
+        this.days=days;
+        this.startDate = startDate;
+    }
+
+
+    public boolean isBefore(){
+        return LocalDate.now().isBefore(startDate);
+    }
+    public int getDays() {
+        return days;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        DateInterval other = (DateInterval) obj;
+        return other.days == this.days &&
+                (other.startDate != null && other.startDate.equals(this.startDate));
+    }
+
+    @Override
+    public String toString() {
+        return "DateInterval{" +
+                "days=" + days +
+                ", startDate=" + startDate +
+                '}';
+    }
+}
---
+++ src/Reservation.java
@@ -0,0 +1,50 @@
+public class Reservation {
+
+    private DateInterval dateInterval;
+    private int pricePerAnimal;
+private Customer customer;
+private OwnedAnimal ownedAnimal;
+
+    public Reservation(DateInterval dateInterval, Customer customer, OwnedAnimal ownedAnimal){
+        this.dateInterval=dateInterval;
+        this.pricePerAnimal=50;
+        this.customer=customer;
+        this.ownedAnimal=ownedAnimal;
+    }
+
+    public DateInterval getDateInterval() {
+        return dateInterval;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+
+    public AnimalList getAnimals(){
+        return animalList;
+    }
+
+    public double getPricePerDay(){
+        return pricePerAnimal;
+    }
+
+    public double getTotalPrice(){
+        return getPricePerDay() * getDateInterval().getDays();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Reservation other = (Reservation) obj;
+        return other.dateInterval == this.dateInterval && other.customer
+                == this.customer && other.ownedAnimal == this.ownedAnimal;
+    }
+
+    public void setPrice(double price){
+        setPrice(price);
+    }
+
+}
